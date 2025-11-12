4ae04d913b898e944a4b0981898ab9cd4421a651
Alberto Araujo <alberto.z.araujo@aluno.senai.br>
2025-11-12 11:47:20 -0300

    :sparkles: feat (index, script, style): Adicionando recurso de capitalização de texto.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 window.document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("btnMaiuscula").addEventListener("click", () => {
-        let inputText = document.querySelector("#input-text").value
-        document.getElementById("resultado").innerHTML = inputText.toUpperCase()
-    })
-    document.getElementById("btnMinuscula").addEventListener("click", () => {
-        let inputText = document.getElementById("input-text").value
-        document.getElementById("resultado").innerHTML = inputText.toLowerCase()
-    })
-})
+  document.getElementById("btnMaiuscula").addEventListener("click", () => {
+    let inputText = document.querySelector("#input-text").value;
+    document.getElementById("resultado").innerHTML = inputText.toUpperCase();
+  });
+  document.getElementById("btnMinuscula").addEventListener("click", () => {
+    let inputText = document.getElementById("input-text").value;
+    document.getElementById("resultado").innerHTML = inputText.toLowerCase();
+  });
+  document.getElementById("btnCapitalize").addEventListener("click", () => {
+    let inputText = document.getElementById("input-text").value.toLowerCase();
+    // let firstLetter = inputText.charAt(0).toUpperCase();
+    inputText = inputText
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    // document.getElementById("resultado").innerHTML = firstLetter+inputText;
+    document.getElementById("resultado").innerHTML = inputText;
+  });
+});
